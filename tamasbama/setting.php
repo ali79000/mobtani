e6b19b1f85ae5391d512dad447bca4cf0835676d
ali79000 <42024747+ali79000@users.noreply.github.com>
2021-02-26 04:06:35 +0330

    Add files via upload

---
+++ tamasbama/setting.php
@@ -0,0 +1,13 @@
+<?php
+if( ! defined('DBHOST'))
+define('DBHOST','sql302.b6b.ir');
+define('DBUSER','b6bi_27932230');
+define('DBPASS','Ali13799731');
+define('DBNAME','b6bi_27932230_computer');
+
+
+$dbHost=DBHOST;
+$dbUser=DBUSER;
+$dbPass=DBPASS;
+$dbName=DBNAME;
+?>
