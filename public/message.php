4ab9e800a7f438e784dbabe86bc127e08eee4b27
ali79000 <42024747+ali79000@users.noreply.github.com>
2021-05-07 05:54:14 +0430

    Delete public directory

--- public/message.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include '__php__.php';	
-$db=new DB();
-$table=messages::find();
-unset($db);
-$alerts=alerts();
-
-include ('views/message.php');
-?>
--- public/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include '__php__.php';	
-$db=new DB();
-$table=Register::find();
-unset($db);
-$alerts=alerts();
-
-include ('views/user.php');
-?>
